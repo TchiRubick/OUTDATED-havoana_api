e6fcfcb5186992e743d67279e659f6486ef2f28e
TchiRubick <tchi.devica@gmail.com>
2020-10-03 23:28:18 +0200

    Ajout default route

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,6 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
-$router->get('/', function ()  {
-    return "Are you lost ?";
-});
-
 $router->get('/key', function() {
     return \Illuminate\Support\Str::random(32);
 });
@@ -23,3 +18,12 @@ $router->post('/authentification', 'AuthController@authentification');
 $router->post('/allProduitActif', 'ProduitController@getAllProduit');
 $router->post('/setTransacSell', 'TransacController@setTransacSell');
 $router->post('/newUser', 'TaskController@sendMailNewUser');
+
+
+// Default route if not matching any route
+$router->get('{slug}', function() {
+    return array(
+        'success' => true,
+        'response' => []
+    );
+});
